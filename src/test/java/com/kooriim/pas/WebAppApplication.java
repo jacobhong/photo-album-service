b7e5629ef906ca51b187cda5adebd209aea507fd
jacob.hong <jacob.hong@gmail.com>
2021-03-04 22:48:29 -0800

    fix test

--- src/test/java/com/kooriim/pas/WebAppApplication.java
+++ src/test/java/com/kooriim/pas/WebAppApplication.java
@@ -4,7 +4,6 @@ import com.kooriim.pas.config.SecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.*;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @ComponentScan(basePackages = {"com.kooriim.pas"}, excludeFilters = {
   @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = SecurityConfig.class)})
@@ -15,10 +14,6 @@ class WebAppApplication {
 
   @Test
   void contextLoads() {
-  }
-  @Bean
-  public WebClient webClient() {
-    return WebClient.builder().build();
   }
 
 }
