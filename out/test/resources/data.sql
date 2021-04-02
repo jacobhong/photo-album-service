aeb3ddf96e930d247f6e41a710237416dad17383
jacob.hong <jacob.hong@gmail.com>
2021-04-01 20:45:24 -0700

    update nginx

--- out/test/resources/data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `users` (`name`, `google_id`, `email`, `created`, `updated`)
-VALUES
-	('test', 'anonymousUser', 'anonymousUser', '2020-02-23 02:48:35', '2020-02-23 02:48:35');
---INSERT INTO `photo` (`id`, `title`, `file_path`, `thumbnail_file_path`, `content_type`, `description`, `google_id`, `created`, `updated`)
---VALUES
---	(1, 'test', 'test', 'test', 'test', 'test', 'anonymousUser', '2020-02-23 02:48:35', '2020-02-23 02:48:35');
---INSERT INTO `photo` (`id`, `title`, `file_path`, `thumbnail_file_path`, `content_type`, `description`, `google_id`, `created`, `updated`)
---VALUES
---	(2, 'test', 'test', 'test', 'test', 'test', 'anonymousUser', '2020-02-23 02:48:35', '2020-02-23 02:48:35');
